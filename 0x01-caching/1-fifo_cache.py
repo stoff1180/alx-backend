f1a0c546c0660f59dbc4fd4689dcc5688c8ddda4
stoff1180 <stoffmustapha1180@gmail.com>
2024-06-28 23:50:17 +0000

    FIFO caching

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Creates FIFOCache class that inherits from BaseCaching"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """ Defines FIFOCache """
+
+    def __init__(self):
+        """ Initializes FIFOCache """
+        self.queue = []
+        super().__init__()
+
+    def put(self, key, item):
+        """ Assigns the item to the dictionary """
+        if key and item:
+            if self.cache_data.get(key):
+                self.queue.remove(key)
+            self.queue.append(key)
+            self.cache_data[key] = item
+            if len(self.queue) > self.MAX_ITEMS:
+                delete = self.queue.pop(0)
+                self.cache_data.pop(delete)
+                print('DISCARD: {}'.format(delete))
+
+    def get(self, key):
+        """ Returns the value associated with the given key """
+        return self.cache_data.get(key)
